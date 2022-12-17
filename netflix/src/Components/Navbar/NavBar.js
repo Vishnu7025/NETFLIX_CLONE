fe31307d68ff5c5b711fae3093d3f38af61255b2
Vishnu  V S <vishnuviswambaran2002@gmail.com>
2022-12-17 10:42:09 +0530

    UI completed

--- netflix/src/Components/Navbar/NavBar.js
+++ netflix/src/Components/Navbar/NavBar.js
@@ -5,6 +5,7 @@ function navbar() {
           <div className='navbar'>
               <img className='logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="Netflix Logo" />
               <img className='avatar' src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png" alt="" />
+              <button className='sign'>Sign In</button>
           </div>
   )
 }
